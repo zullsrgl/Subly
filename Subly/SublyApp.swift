7f2586f39dbb095da6b3b1d4e628da5c545dfea4
zulalsarioglu <zulal.sariogluu@gmail.com>
2026-03-29 18:12:58 +0300

    Initial Commit

---
+++ Subly/SublyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SublyApp.swift
+//  Subly
+//
+//  Created by Zülal Sarıoğlu on 29.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SublyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
